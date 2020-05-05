8a88bdc87155dcedd887ee5417c75f74dba5b2a6
Shimaa <shimaashoukry16@gmail.com>
2020-05-05 04:41:43 +0200

    Create needed Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { DepartmentComponent } from './components/department/department.component';
+import { RightSideComponent } from './components/right-side/right-side.component';
+import { LeftSideComponent } from './components/left-side/left-side.component';
+import { DetailsComponent } from './components/details/details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    DepartmentComponent,
+    RightSideComponent,
+    LeftSideComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
